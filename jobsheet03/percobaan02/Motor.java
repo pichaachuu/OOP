d4e7c49a5e3641ee8490791b5b4ddbb6852a1d55
Nafisah Aliyah <nafisahaliyah01@gmail.com>
2025-09-11 09:11:46 +0700

    JOBSHEET 03

---
+++ jobsheet03/percobaan02/Motor.java
@@ -0,0 +1,55 @@
+package jobsheet03.percobaan02;
+
+public class Motor {
+    private String platNomor;
+    private boolean isMesinOn;
+    private int kecepatan;
+
+    public String getPlatNomor(){
+        return platNomor;
+    }
+
+    public void setPlatNomor(String platNomor){
+        this. platNomor = platNomor;
+    }
+
+    public boolean isIsMesinOn(){
+        return isMesinOn;
+    }
+
+    public void setIsMesinOn(boolean isMesinOn){
+        this.isMesinOn = isMesinOn;
+    }
+
+    public int getKecepatan(){
+        return kecepatan;
+    }
+
+    public void setKecepatan(int kecepatan){
+        if (kecepatan < 0) {
+            System.out.println("Kecepatan tidak boleh negatif, silahkan input angka yang benar");
+            return;
+        }
+        if (!this.isMesinOn && kecepatan > 0) {
+            System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off");
+        } else if (kecepatan > 100) {
+            System.out.println("Batas maksimum kecepatan adalah 100");
+            this.kecepatan = 100;
+        } else {
+            this.kecepatan = kecepatan;
+        }
+    }
+
+    public void displayStatus(){
+        System.out.println("Plat Nomor: " + this.platNomor);
+
+        if (isMesinOn) {
+            System.out.println("Mesin On");
+        } else {
+            System.out.println("Mesin Off");
+        }
+
+        System.out.println("Kecepatan: " + this.kecepatan);
+        System.out.println("===========================");
+    }
+}
