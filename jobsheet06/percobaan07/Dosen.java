95cce241b0c6ec06ed9990700094a7d40c4d090a
Nafisah Aliyah <nafisahaliyah01@gmail.com>
2025-10-08 22:43:51 +0700

    Jobsheet 6

--- jobsheet06/percobaan07/Dosen.java
+++ jobsheet06/percobaan07/Dosen.java
@@ -2,15 +2,18 @@ package jobsheet06.percobaan07;
 
 public class Dosen extends Pegawai {
     public String nidn;
+    public String matakuliah;
 
-    public Dosen(String nip, String nama, double gaji, String nidn){
+    public Dosen(String nip, String nama, double gaji, String nidn, String matakuliah){
         super(nip, nama, gaji);
         this.nidn = nidn;
+        this.matakuliah = matakuliah;
     }
 
     public String getAllInfo(){
         String info = super.getInfo();
         info += "NIDN : " + nidn;
+        info += "\nMata Kuliah : " + matakuliah;
         return info;
     }
 }
--- jobsheet06/percobaan07/InheritanceDemo.java
+++ jobsheet06/percobaan07/InheritanceDemo.java
@@ -2,7 +2,9 @@ package jobsheet06.percobaan07;
 
 public class InheritanceDemo {
    public static void main(String[] args) {
-        Dosen dosen1 = new Dosen("34329837", "Yansy Ayuningtyas", 3000000, "19894324837");
+        Dosen dosen1 = new Dosen("34329837", "Yansy Ayuningtyas", 3000000, "19894324837", "PBO");
         System.out.println(dosen1.getAllInfo());
+        Dosen dosen2 = new Dosen("34329838", "Ahmad Fauzan", 3500000, "19894324838", "Basis Data");
+        System.out.println(dosen2.getAllInfo());
    } 
 }
