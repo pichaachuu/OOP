ac8150541fd2da9e360af6ea34b5994e7fbd1754
Nafisah Aliyah <nafisahaliyah01@gmail.com>
2025-10-30 07:33:48 +0700

    JOBSHEET 9

--- jobsheet09/percobaan02/AbstractClassDemo.java
+++ jobsheet09/percobaan02/AbstractClassDemo.java
@@ -6,5 +6,10 @@ public class AbstractClassDemo {
         lebah1.cetakInfo();
         lebah1.bergerak();
         lebah1.bernapas();
+
+        Ular ular1 = new Ular(false, 1, "daging", "hutan");
+        ular1.cetakInfo();
+        ular1.bergerak();
+        ular1.bernapas();
     }
 }
